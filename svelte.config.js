5e9b0847a75c971c380235eecee36a5c7a7ae53b
Stephen Osentoski <stosento@umich.edu>
2024-04-26 19:50:50 -0400

    Updating with vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,19 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import vercel from '@sveltejs/adapter-vercel';
 import path from 'path';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-
+	preprocess: vitePreprocess(),
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: vercel(),
 		alias: {
 			$lib: path.resolve('./src/lib')
 		}
-	},
-	preprocess: vitePreprocess()
+	}
 };
 
 export default config;
